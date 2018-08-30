8d7706675206ca6584fe7e2769bb19f11c8a4c8e
divyadsharm <dvsharma129@gmail.com>
2018-08-30 21:17:48 +0530

    adding burger-builder

---
+++ src/components/Burger/BuildControl/BuildControl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './BuildControl.css'
+
+const buildControl = (props) => (
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}> {props.label} </div>
+    <div className={classes.Less} onClick={props.removed} disabled={props.disabled}> less </div>
+    <div className={classes.More} onClick={props.added}> more </div>
+  </div>
+)
+
+export default buildControl;
